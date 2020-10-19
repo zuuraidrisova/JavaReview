2f17cfc617bceb3bcd7fe96495fa9ea0eff9ef52
Zuura Idrisova <zuuraidrisova@gmail.com>
2020-10-19 12:53:56 -0400

    Java Review

---
+++ src/day54_Maps/SumOfDigits.java
@@ -0,0 +1,37 @@
+package day54_Maps;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+        //Write a method that can return the sum of the digits in a string
+
+        String str = "a1b2c3";
+
+        int sum = sumOfDigits(str);
+
+        System.out.println("sum = " + sum);
+    }
+
+    public static int sumOfDigits(String str){
+
+       char [] ch = str.toCharArray();
+
+       int sum = 0;
+
+       for (int i = 0; i < ch.length; i++){
+
+           if(Character.isDigit(ch[i])) {
+
+               int number = Integer.parseInt("" + ch[i]);
+
+               sum += number;
+           }
+       }
+
+       return sum;
+    }
+
+
+}
+
